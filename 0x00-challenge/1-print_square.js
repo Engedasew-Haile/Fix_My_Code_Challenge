e49a8c0cd33bf2e5957d20346c0e9a0c788c9eb8
Engedasew-Haile <engedasewhaile@gmail.com>
2023-07-14 07:56:35 -0700

    update

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 10) {
-    process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+let n = 5; // you can take input from prompt
+let string = "";
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    string += "#";
+  }
+  string += '\n';
 }
-
-size = parseInt(process.argv[2], 10)
-
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
-}
+console.log(string);
