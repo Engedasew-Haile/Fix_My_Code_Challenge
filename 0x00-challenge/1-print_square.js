7207914debd2d55b0acc3827a06b70d0eb390437
Engedasew-Haile <engedasewhaile@gmail.com>
2023-07-14 08:15:21 -0700

    update

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -9,9 +9,9 @@
 
 
 if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
+ //   process.stderr.write("Missing argument\n");
+ //   process.stderr.write("Usage: ./1-print_square.js <size>\n");
+ //   process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1)
 }
 
